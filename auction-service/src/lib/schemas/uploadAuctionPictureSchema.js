90e9c628381f7fe183a9b1a7a2445215df2bd9af
NileshSanyal <nil2take1@gmail.com>
2021-02-09 15:13:31 +0530

    initial commit

---
+++ auction-service/src/lib/schemas/uploadAuctionPictureSchema.js
@@ -0,0 +1,15 @@
+
+const schema = {
+    properties: {
+        body: {
+            type: 'string',
+            minLength: 1,
+            pattern: '\=$'
+        },
+    },
+    required: [
+        'body',
+    ]
+};
+
+export default schema;
